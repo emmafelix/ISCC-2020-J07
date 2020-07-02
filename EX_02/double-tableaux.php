0ec4b97f0d7bfee206123cb010e089b3984bc525
emmafelix <emma.felix@iseg.fr>
2020-07-02 16:38:39 +0200

    EX-02-Fin

--- EX_02/double-tableaux.php
+++ EX_02/double-tableaux.php
@@ -2,9 +2,9 @@
 $panier= array(
     array('T-shirt rouge',15.50,5),
     array('T-shirt vert',15.50,6),
-    array('T-shirt agrent',15.50,6),
+    array('T-shirt agrent',15.50,8),
     array('Short bleu',16.50,5),
-    array('Short vert',19.99,10),
+    array('Short vert',19.99,5),
     array('Veste argent',19.99,10),
     array('Veste argent',35,3)
 );
